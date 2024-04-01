80bbf72b5edfda750751416e0a997c5b2b31f944
Lars Kristian Haga <larskhaga@gmail.com>
2024-04-01 12:08:29 +0200

    Updated user object

--- src/main/java/no/haga/models/User.java
+++ src/main/java/no/haga/models/User.java
@@ -1,13 +1,16 @@
 package no.haga.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
     private String fullName;
     private String firstName;
     private String lastName;
     private String address;
+    private String postCode;
     private String city;
     private String email;
     private String password;
