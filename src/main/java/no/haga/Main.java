00592a835650912bf2fe1d18673d9b226640056c
Lars Kristian Haga <larskhaga@gmail.com>
2025-01-12 21:45:55 +0100

    Renamed `PlaywrightThread` to `Main`

--- src/main/java/no/haga/Main.java
+++ src/main/java/no/haga/Main.java
@@ -1,24 +1,44 @@
 package no.haga;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import com.microsoft.playwright.options.AriaRole;
-import lombok.SneakyThrows;
+import lombok.extern.java.Log;
+import no.haga.models.User;
 
-public class Main {
-    public static void main(String[] args) {
-        runPlaywright();
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Log
+public class Main implements Runnable {
+
+    private static final int NUMBER_OF_THREADS = 2;
+    private final User user;
+
+    public Main(User user) {
+        this.user = user;
     }
 
-    @SneakyThrows
-    private static void runPlaywright() {
-        var mockUsers = Helpers.getMockData();
+    public static void main(String[] args) throws IOException {
+        List<User> mockUsers = Helpers.getMockData();
 
-        var playwright = Playwright.create();
+        log.info("Starting spam...");
 
-        mockUsers.forEach(user -> {
-            System.out.println(STR."Starting test for user: \{user.getFullName()}");
+        try (ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS)) {
+            for (User user : mockUsers) {
+                executor.submit(new Main(user));
+            }
+            executor.shutdown();
+        }
+    }
+
+    @Override
+    public void run() {
+        try (Playwright playwright = Playwright.create()) {
+            log.info("Starting test for user: " + user.getFullName());
 
             var fullName = user.getFullName();
             var address = user.getAddress();
@@ -27,20 +47,16 @@ public class Main {
             var phoneNumber = user.getMobileNumber();
             var creditCardNumber = user.getCreditCardNumber();
 
-            var browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
-            var context = browser.newContext();
-            var page = context.newPage();
+            var launchOptions = new BrowserType.LaunchOptions()
+                    .setHeadless(false)
+                    .setTimeout(30)
+                    .setArgs(List.of("--start-maximized", "--window-position=0,0"));
 
-            var url = "";
+            try (Browser browser = playwright.webkit().launch(launchOptions)) {
+                BrowserContext context = browser.newContext(new Browser.NewContextOptions().setDeviceScaleFactor(3).setHasTouch(true).setIsMobile(true).setUserAgent("Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Mobile/15E148 Safari/604.1").setViewportSize(390, 664));
 
-            // Navigate to the URL
-            page.navigate(url);
-
-            // Examples on how to interact with the page
-            page.frameLocator("iframe[title=\"XXX\"]").locator("button[name=\"XXX\"]").fill("XXX");
-            page.getByRole(AriaRole.BUTTON, new Page.GetByRoleOptions().setName("XXX")).fill("XXX");
-            page.getByPlaceholder("XXX").fill("XXX");
-            page.getByLabel("XXX").fill("XXX");
-        });
+                var page = context.newPage();
+            }
+        }
     }
 }
