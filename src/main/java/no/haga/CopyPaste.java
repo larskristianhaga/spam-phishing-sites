56fdf3540c5c7ccc800053e9d48cd06382aec836
Lars Kristian Haga <LarsKHaga@gmail.com>
2025-02-09 21:12:40 +0100

    Added new class to make it easier to copy paste code

---
+++ src/main/java/no/haga/CopyPaste.java
@@ -0,0 +1,12 @@
+package no.haga;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.*;
+import no.haga.models.User;
+
+public class CopyPaste {
+
+    public static void copyPasteCode(Page page, User user) {
+
+    }
+}
