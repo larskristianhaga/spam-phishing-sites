6e8756cfcd282b52f14ce67c9e963e6c732bb625
Lars Kristian Haga <LarsKHaga@gmail.com>
2025-02-09 21:04:05 +0100

    Change default config

--- src/main/java/no/haga/config/Config.java
+++ src/main/java/no/haga/config/Config.java
@@ -1,7 +1,6 @@
 package no.haga.config;
 
 import com.microsoft.playwright.BrowserType;
-import java.util.List;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -9,8 +8,7 @@ public class Config {
 
     public BrowserType.LaunchOptions getLaunchOptions() {
         return new BrowserType.LaunchOptions()
-                .setHeadless(true)
-                .setTimeout(30)
-                .setArgs(List.of("--start-maximized", "--window-position=0,0"));
+                .setHeadless(false)
+                .setSlowMo(500);
     }
 }
